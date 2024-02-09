cbeda173ded5adb5a0856d2333b1c69cfa4f7d60
Ivan Haydarov <navi1435@gmail.com>
2024-02-10 03:01:30 +0700

    webflux app modified

--- slow-service/src/main/java/ru/flamexander/product/details/service/controllers/ProductDetailsController.java
+++ slow-service/src/main/java/ru/flamexander/product/details/service/controllers/ProductDetailsController.java
@@ -1,10 +1,9 @@
 package ru.flamexander.product.details.service.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.flamexander.product.details.service.dtos.ProductDetailsDto;
+
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/v1/details")
@@ -12,9 +11,12 @@ public class ProductDetailsController {
     @GetMapping("/{id}")
     public ProductDetailsDto getProductDetailsById(@PathVariable Long id) throws InterruptedException {
         if (id > 100) {
-            throw new RuntimeException();
+            throw new NoSuchElementException("There is only 100 product details!");
+        }
+        if(id%2==1){
+           return null;
         }
         Thread.sleep(2500 + (int)(Math.random() * 2500));
-        return new ProductDetailsDto(id, "Product description..");
+        return new ProductDetailsDto(id, "Product description: product №" + id);
     }
 }
